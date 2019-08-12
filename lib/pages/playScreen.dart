82719c2e09749becb169cdc3f2dde2a563889f97
RajeshMuthyal <rajesh.muthyala@tarento.com>
2019-08-12 09:50:37 +0530

    Icon added with profile page changed

--- lib/pages/playScreen.dart
+++ lib/pages/playScreen.dart
@@ -1,130 +1,147 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:flutter_share_me/flutter_share_me.dart';
 import 'package:Minesweeper/pages/Settings.dart';
 import 'package:Minesweeper/widget/game_board.dart';
-
+import 'package:flutter/material.dart';
+import 'dart:async';
 void main() {
   runApp(MaterialApp(
     home: RunScreen(),
   ));
 }
-
-
 class RunScreen extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return RunScreenState();
   }
 }
-
 class RunScreenState extends State<RunScreen> {
- var Choice = 'Medium';
+  var Choice = 'Medium';
   @override
   void initState() {
     super.initState();
-    loadData();
   }
 
-  Future<Timer> loadData() async {
-    return new Timer(Duration(seconds: 5), onDoneLoading);
-  }
-
-
-  onDoneLoading() async {
-//    Navigator.of(context).pushReplacement(
-//        MaterialPageRoute(builder: (context) => LoginPage()));
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-          fit: StackFit.expand,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Minesweeper"),
+      ),
+      body:
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 85.0,
-                  child: IconButton(
-                    icon: Icon(Icons.play_circle_filled, size: 50.0, color: Colors.deepOrange),
-                      onPressed: () {
+            CircleAvatar(
+                backgroundColor: Colors.black38,
+                radius: 40.0,
+                child:
+                ButtonTheme(
+                  buttonColor: Colors.black38,
+                  // minWidth: 100.0,
+                  height: 150.0,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(50.0)),
+                    onPressed: () {
                       setState(() {
                         Navigator.push(
-                         context,
-                          MaterialPageRoute(
-                          builder: (context) => GameBoard(Level: Choice),
-                        )
+                            context,
+                            MaterialPageRoute(builder: (context)=>GameBoard(Level: Choice),
+                            )
                         );
                       });
-                      }
-                  )
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                ),
-                Text(
-                  'Play Mine Sweeper',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24.0
+                    },
+                    child: Image.asset('assets/images/playred.png'),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 100.0),
-                ),
-                Row(
-                  children: <Widget>[
-                    _ShareFacebook(),
-                    Padding(
-                      padding: EdgeInsets.only(right: 10.0),
-                    ),
-                    _SharewhatsApp(),
-                    Padding(
-                      padding: EdgeInsets.only(right: 10.0),
-                    ),
-                    _ShareTwitter(),
-                    Padding(
-                      padding: EdgeInsets.only(right: 10.0),
-                    ),
-                    _Settings(),
-                  ],
-
                 )
-              ],
             ),
-          ]
+            // Image.asset('assets/giphy.gif'),
+            Padding(
+                padding: EdgeInsets.all(15),
+                child: Text('Play Mine Sweeper',  style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w800,
+                  fontFamily: 'Roboto',
+                  letterSpacing: 0.5,
+                  fontSize: 25,
+                ),)
+            )
+          ],
+        ),
+      ),
+      bottomSheet:Container(
+        color: Colors.white,
+        // margin: const EdgeInsets.only(top: 0, left: 50, right: 50,bottom:0),
+        height:145,
+        child: Center(
+//             mainAxisSize: MainAxisSize.max,
+//             mainAxisAlignment: MainAxisAlignment.center,
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _ShareFacebook(),
+                _SharewhatsApp(),
+                _ShareTwitter(),
+                _Settings(),
+              ],
+            )
+        ),
       ),
     );
   }
-
-
   _ShareFacebook() {
     return Container(
-      height: 60.0,
-      width: 60.0,
+      height: 70.0,
+      width: 70.0,
 //      padding: EdgeInsets.only(right: 40.0),
       decoration: BoxDecoration(
-        color: Colors.green,
+        color: Colors.white,
         shape: BoxShape.circle,
       ),
       child: Center(
           child: new Image(
-              image: new AssetImage('assets/images/facebook_circle.png'),
+              image: new AssetImage('assets/images/facebook.png'),
               color: null, width: 100, height: 100)
       ),
     );
   }
-
   _SharewhatsApp() {
     return Container(
-      height: 60.0,
-      width: 60.0,
+      height: 70.0,
+      width: 70.0,
       decoration: BoxDecoration(
 //        color: Colors.green,
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+          child: new Image(image: new AssetImage('assets/images/whatapp.png'),
+              color: null, width: 100, height: 100)
+      ),
+    );
+  }
+  _Settings() {
+    return Container(
+      height: 70.0,
+      width: 70.0,
+      decoration: BoxDecoration(
+//      color: Colors.green,
+        shape: BoxShape.circle,
+//        borderRadius: BorderRadius.all(Radius.circular(25.0)),
+      ),
+      child: GestureDetector(
+          onTap: () => SettingsPageNav(),
+          child: new Image(image: new AssetImage('assets/images/setting.png'),
+              color: null, width: 100, height: 100)
+      ),
+    );
+  }
+  _ShareTwitter() {
+    return Container(
+      height: 70.0,
+      width: 70.0,
+      decoration: BoxDecoration(
+//      color: Colors.green,
         shape: BoxShape.circle,
       ),
       child: Center(
@@ -133,42 +150,6 @@ class RunScreenState extends State<RunScreen> {
       ),
     );
   }
-
-  _Settings() {
-    return Container(
-
-      height: 70.0,
-      width: 70.0,
-      decoration: BoxDecoration(
-//      color: Colors.green,
-        shape: BoxShape.circle,
-
-//        borderRadius: BorderRadius.all(Radius.circular(25.0)),
-      ),
-      child: GestureDetector(
-          onTap: () => SettingsPageNav(),
-            child: new Image(image: new AssetImage('assets/images/settings.png'),
-              color: null, width: 100, height: 100)
-      ),
-    );
-  }
-  _ShareTwitter() {
-    return Container(
-      height: 80.0,
-      width: 80.0,
-      decoration: BoxDecoration(
-//      color: Colors.green,
-        shape: BoxShape.circle,
-      ),
-      child: Center(
-          child: new Image(image: new AssetImage('assets/images/whatsapp.png'),
-              color: null, width: 100, height: 100)
-      ),
-    );
-  }
-
-
-
   SettingsPageNav() {
     Navigator.push(
       context,
@@ -178,14 +159,3 @@ class RunScreenState extends State<RunScreen> {
     );
   }
 }
-
-//class HomeScreen extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      body: Center(
-//        child: Text('Welcome to Homescreen', style: TextStyle(fontSize: 24.0),),
-//      ),
-//    );
-//  }
-//}
